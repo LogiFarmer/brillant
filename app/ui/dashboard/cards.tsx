9ccedfd1975fa24ec6c1f74861d024ae3c46d54d
LogiFarmer <logicielfarmer@gmail.com>
2024-04-11 20:13:07 -0400

    add auth code

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -36,28 +36,28 @@ export function Card({
 }) {
 
   return (
-    <div className="rounded-xl overflow-visible bg-gray-50 p-2 shadow-sm">
-      <div className="relative mx-auto bg-white-50 shadow-md rounded-md flex items-center gap-2">
+    <div className="rounded-xl overflow-visible bg-gray-50 p-2 shadow-sm max-w-min min-w-max">
+      <div className="relative bg-white-50 shadow-md rounded-md flex gap-2 max-w-min min-w-max">
         <Image alt="" className="absolute -left-6 -top-6 w-20 h-20 rounded-full shadow-lg" 
-          src="/stores/walmart-logo-474.png"
-          width={0}
-          height={0}
-          sizes="100vw"
-        />
-        <div className="flex flex-col p-2">
+            src="/stores/walmart-logo-474.png"
+            width={0}
+            height={0}
+            sizes="100vw"
+          />
+        <div className="flex flex-col p-1">
           <Image
             src={image_url}
             width={0}
             height={0}
             sizes="100vw"
-            className="w-48 h-auto"
+            className="w-40 h-auto"
             alt="test"
           />
-          <p className="truncate flex p-2 text-lg font-medium">{title}</p>
-          <p className="truncate rounded-xl px-2 text-rose-900 text-right font-bold text-4xl">
+          <p className="whitespace-nowrap flex p-0 text-md font-medium">{title}</p>
+          <p className="px-2 text-rose-900 text-right font-bold text-3xl">
             {formatCurrency(value) + "/" + unit}
           </p>
-          <p className='text-gray-500'>Ends {expire_date}</p>
+          <p className='text-gray-500 text-sm'>Ends {expire_date}</p>
         </div>
       </div>
     </div>
